b5a7bd900138056f5cd2facedfdf3da82d4dac85
lauyung <ljy123liu@gmail.com>
2016-06-20 09:49:13 +0800

    Initial Commit

---
+++ LYBannerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYBannerView
+//
+//  Created by LIUYONG on 16/6/20.
+//  Copyright © 2016年 WanJianTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYBannerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYBannerView
+//
+//  Created by LIUYONG on 16/6/20.
+//  Copyright © 2016年 WanJianTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
